cf7f1bc829e4b9f1bf3801e230ad41acd3f1db9d
14750040 <14750040@me.mcu.edu.tw>
2026-04-13 11:58:58 +0800

    week08

---
+++ week08/week08-5c.cpp
@@ -0,0 +1,16 @@
+// week08-5c.cpp SOIT107_Base_013
+#include <stdio.h>
+int main()
+{
+	int a;
+	scanf("%d",&a);
+	printf("%d", (a-1)%7);
+	//1: 0
+	//2: 1
+	//3: 2
+	//4: 3
+	//5: 4
+	//6: 5
+	//7: 6
+	//8: 0
+}
